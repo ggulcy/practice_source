fbb10f311dea8011209ae0ccae1da0842501bd25
parkchanhyeok <ggulcy@gmail.com>
2020-03-04 18:21:05 +0900

    2016 year update

--- CodingTest/app/src/test/java/com/ggulcy/codingtest/이천십육년.kt
+++ CodingTest/app/src/test/java/com/ggulcy/codingtest/이천십육년.kt
@@ -1,2 +1,35 @@
 package com.ggulcy.codingtest
 
+import org.junit.Test
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.random.Random
+
+class 이천십육년 {
+
+    @Test
+    fun main() {
+        var a = 1
+        var b = 3
+        var answer = ""
+
+        var arrWeek = arrayOf("FRI","SAT","SUN","MON","TUE","WED","THU")
+        var arrLastDate = arrayOf(31,29,31,30,31,30,31,31,30,31,30,31)
+        var allDay = 0
+        for(i in 0 until a-1){
+            allDay += arrLastDate[i]
+        }
+        allDay += (b-1)
+
+        answer = arrWeek[(allDay%arrWeek.size)]
+
+
+        print(answer)
+
+
+    }
+
+
+}
+
+
