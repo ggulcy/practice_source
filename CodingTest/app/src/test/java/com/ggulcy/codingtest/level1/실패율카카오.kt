9a2464a7f975a429c60f6618fcce61d7239e0f4a
parkchanhyeok <ggulcy@gmail.com>
2020-03-18 21:13:10 +0900

    실패율 진행 중

--- CodingTest/app/src/test/java/com/ggulcy/codingtest/level1/실패율카카오.kt
+++ CodingTest/app/src/test/java/com/ggulcy/codingtest/level1/실패율카카오.kt
@@ -2,9 +2,6 @@ package com.ggulcy.codingtest.level1
 
 import org.junit.Test
 
-import org.junit.Assert.*
-import java.util.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -14,36 +11,37 @@ class 실패율카카오 {
     @Test
     fun main() {
 
-        var stages = intArrayOf(2,1,2,6,2,4,3,3)
+        var stages = intArrayOf(2, 1, 2, 6, 2, 4, 3, 3)
         var N = 5
 
 
-        var rates = mutableMapOf<Int,Double>()
+        var rates = mutableMapOf<Int, Double>()
 
-        for(i in 1 .. N){
+        for (i in 1..N) {
             var clear = 0
             var cur = 0
             stages.forEach {
-                if(it >= i) {
+                if (it >= i) {
                     clear++
-                    if(it == i) cur++
+                    if (it == i) cur++
                 }
             }
-            rates.put(i,cur.toDouble()/clear.toDouble())
+            rates.put(i, cur.toDouble() / clear.toDouble())
         }
 
 //        var result = rates.toList().sortedBy { (_, value) -> value}.reversed().toMap()
 
-        var sorted = rates.toSortedMap().values
+        var sorted = rates.toSortedMap().values.sorted().reversed()
+
+        var answer = mutableListOf<Int>()
+        sorted.forEach {
+            for (i in 1..rates.values.size) {
+                if (it == rates[i] && answer.indexOf(i) == -1) answer.add(i)
+            }
+        }
 
 
-
-//        with(rates){
-//            sort()
-//            reverse()
-//        }
-
-//        result.forEach { println(it) }
+        answer.toIntArray()
 
 
     }
